985c4d4fda7c07c7b76175c48991eb752b623866
nathaliaescarlate <nathaliaescarlate@hotmail.com>
2018-11-02 18:22:29 -0300

    nath_edits

--- core/models.py
+++ core/models.py
@@ -36,25 +36,50 @@ class DisciplinaOfertada(models.Model):
     professor = models.ForeignKey(Professor, on_delete=models.PROTECT)
 
     class Meta:
-        unique_together = ('curso', 'disciplina', 'turma', 'ano', 'semestre')
+       unique_together = ('curso', 'disciplina', 'turma', 'ano', 'semestre')
 
 class SolicitacaoMatricula(models.Model):
     pass
 
 class Atividade(models.Model):
 
-    pass
+    titulo    = models.TextField(max_length=30) 
+    descricao = models.TextField(max_length=100) 
+    conteudo  = models.TextField(max_length=8000) 
+    tipo      = models.TextField(max_length=15) 
+    extras    = models.TextField(max_length=100) 
+    
+    professor = models.ForeignKey(Professor, on_delete=models.PROTECT)
+     
 
 class AtividadeVinculada():
 
-    pass
+    atividade     = models.ForeignKey(Atividade, on_delete=models.PROTECT)
+    professor     = models.ForeignKey(Professor, on_delete=models.PROTECT)
+    dofertada     = models.ForeignKey(DisciplinaOfertada, on_delete=models.PROTECT)
+
+    rotulo        = models.TextField(max_length=500)  
+    status        = models.TextField(max_length=15)
+    dtiniresposta = models.DateTimeField()
+    dtfimresposta = models.DateTimeField()
+
 
 class EntregaAtividade():
 
-    professor = models.ForeignKey(Professor, on_delete=models.PROTECT)
-    atividade = models.ForeignKey(Atividade, on_delete=models.PROTECT)
-    disciplina_ofertada = models.ForeignKey(DisciplinaOfertada, on_delete=models.PROTECT)
+    #professor = models.ForeignKey(Professor, on_delete=models.PROTECT)
+    #atividade = models.ForeignKey(Atividade, on_delete=models.PROTECT)
+    #disciplina_ofertada = models.ForeignKey(DisciplinaOfertada, on_delete=models.PROTECT)
+    pass
 
 class Mensagem(models.Model):
+ 
+    aluno      = models.ForeignKey(Aluno, on_delete=models.PROTECT)
+    professor  = models.ForeignKey(Professor, on_delete=models.PROTECT)
 
-    pass
+    assunto    = models.TextField(max_length=100) 
+    referencia = models.TextField(max_length=100) 
+    conteudo   = models.TextField(max_length=500) 
+    status     = models.TextField(max_length=50) 
+    dtenvio    = models.DateField(default=date(year=1900, month=1, day=1))
+    dtresposta = models.DateField(default=date(year=1900, month=1, day=1))
+    resposta   = models.TextField(max_length=500)
