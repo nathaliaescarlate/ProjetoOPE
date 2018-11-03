96506ce50933025361f874048c7661a60234c49e
nathaliaescarlate <nathaliaescarlate@hotmail.com>
2018-11-03 02:07:11 -0300

    new_models_n

--- core/models.py
+++ core/models.py
@@ -5,96 +5,123 @@ from datetime import date
 
 class Usuario(models.Model):
 
-    nome = models.TextField(max_length=50)
-    email = models.TextField(max_length=30)
-    celular = models.TextField(max_length=25)
     dt_expiracao = models.DateField(default=date(year=1900, month=1, day=1))
-    login = models.TextField(max_length=30, unique=True)
-    senha = models.TextField(max_length=8) #TIRAR OU NÃO O CHECK??? PROF NÃO DEU EM AULA
+    login        = models.TextField(max_length=30, unique=True)
+    senha        = models.TextField(max_length=8) 
 
 class Coordenador(models.Model):
-    pass
+    
+    usuario = models.ForeignKey(Usuario, on_delete=models.PROTECT)
+
+    nome    = models.TextField(max_length=50)
+    email   = models.EmailField(max_length=50, unique=True)  
+    celular = models.TextField(max_length=11, unique=True)
 
 class Aluno(models.Model):
     
-    nome = models.TextField(max_length=50)
-    email = models.TextField(max_length=50, unique=True)
+    usuario = models.ForeignKey(Usuario, on_delete=models.PROTECT)
+
+    nome    = models.TextField(max_length=50)
+    email   = models.EmailField(max_length=50, unique=True)
     celular = models.TextField(max_length=11, unique=True)
-    ra = models.TextField(max_length=7, unique=True)
-    foto = models.TextField(max_length=100)
+    ra      = models.TextField(max_length=7, unique=True)
+    foto    = models.TextField(max_length=100)
 
-class Professor(Usuario):
-    nome = models.TextField(max_length=50)
-    email = models.TextField(max_length=30)
-    celular = models.TextField(max_length=11)
+class Professor(models.Model):
+
+    usuario = models.ForeignKey(Usuario, on_delete=models.PROTECT)
+
+    nome    = models.TextField(max_length=50)
+    email   = models.EmailField(max_length=50, unique=True)
+    celular = models.TextField(max_length=11, unique=True)
     apelido = models.TextField(max_length=50)
-
-    idusuario = models.ForeignKey(Usuario, on_delete=models.PROTECT)
-
-class Disciplina(models.Model):
-    
-    nome = models.TextField(max_length=30, unique=True)
-    data = models.DateField(default=date(year=1900, month=1, day=1))
-    status = models.TextField(max_length=10)
-    plano_ensino = models.TextField(max_length=1000)
-    carga_horaria = models.SmallIntegerField()
-    competencias = models.TextField(max_length=1000)
-    habilidades = models.TextField(max_length=1000)
-    ementa = models.TextField(max_length=1000)
-    conteudo_programatico = models.TextField(max_length=1000)
-    bibliografia_basica = models.TextField(max_length=1000)
-    bibliografia_complementar = models.TextField(max_length=1000)
-    percentual_pratico = models.SmallIntegerField()
-    percentual_teorico = models.SmallIntegerField()
-    coordenador = models.ForeignKey(Coordenador, on_delete=models.PROTECT)
+  
 
 class Curso(models.Model):
 
-    curso = models.TextField(max_length=50)
+    curso = models.TextField(max_length=50, unique=True)
+
+class Disciplina(models.Model):
     
+    coordenador               = models.ForeignKey(Coordenador, on_delete=models.PROTECT)
+
+    nome                      = models.TextField(max_length=30, unique=True)
+    data                      = models.DateField(default=date.today)
+    status                    = models.TextField(max_length=10)
+    plano_ensino              = models.TextField(max_length=1000)
+    carga_horaria             = models.SmallIntegerField()
+    competencias              = models.TextField(max_length=1000)
+    habilidades               = models.TextField(max_length=1000)
+    ementa                    = models.TextField(max_length=1000)
+    conteudo_programatico     = models.TextField(max_length=1000)
+    bibliografia_basica       = models.TextField(max_length=1000)
+    bibliografia_complementar = models.TextField(max_length=1000)
+    percentual_pratico        = models.SmallIntegerField()
+    percentual_teorico        = models.SmallIntegerField()
+       
 class DisciplinaOfertada(models.Model):
-    professor = models.ForeignKey(Professor, on_delete=models.PROTECT)
+
+    professor         = models.ForeignKey(Professor, on_delete=models.PROTECT)
+    coordenador       = models.ForeignKey(Coordenador, on_delete=models.PROTECT)
+    disciplina        = models.ForeignKey(Disciplina, on_delete=models.PROTECT)
+    curso             = models.ForeignKey(Curso, on_delete=models.PROTECT)
+
+    dtiniciomatricula = models.DateTimeField(default=datetime.now, blank=True, null=True)
+    dtfimmatricula    = models.DateTimeField(default=datetime.now, blank=True, null=True)
+    ano               = models.SmallIntegerField()
+    semestre          = models.SmallIntegerField()
+    turma             = models.TextField(max_length=1)
+    metodologia       = models.TextField(max_length=1000, null=True)
+    recursos          = models.TextField(max_length=1000, null=True)
+    criterioavaliacao = models.TextField(max_length=1000,, null=True)
+    planosdeaulas     = models.TextField(max_length=1000,, null=True)
 
     class Meta:
        unique_together = ('curso', 'disciplina', 'turma', 'ano', 'semestre')
 
 class SolicitacaoMatricula(models.Model):
-    
-    #dtsolicitacao = models.datetime
-    status = models.TextField(max_length=15)
+      
+    aluno              = models.ForeignKey(Aluno, on_delete=models.PROTECT)
+    disciplinaofertada = models.ForeignKey(DisciplinaOfertada, on_delete=models.PROTECT)
+    coordenador        = models.ForeignKey(Coordenador, on_delete=models.PROTECT)
 
-    aluno = models.ForeignKey(Aluno, on_delete=models.PROTECT)
-    iddisciplinaofertada = models.ForeignKey(DisciplinaOfertada, on_delete=models.PROTECT)
-    idcoordenador = models.ForeignKey(Coordenador, on_delete=models.PROTECT)
+    dtresposta         = models.DateTimeField(default=datetime.now, blank=True)
+    status             = models.TextField(max_length=15)
 
 class Atividade(models.Model):
 
+    professor = models.ForeignKey(Professor, on_delete=models.PROTECT)
+
     titulo    = models.TextField(max_length=30) 
-    descricao = models.TextField(max_length=100) 
+    descricao = models.TextField(max_length=100, null=True, unique=True) 
     conteudo  = models.TextField(max_length=8000) 
     tipo      = models.TextField(max_length=15) 
-    extras    = models.TextField(max_length=100) 
-    
-    professor = models.ForeignKey(Professor, on_delete=models.PROTECT)
-     
+    extras    = models.TextField(max_length=100, null=True)    
 
-class AtividadeVinculada():
+class AtividadeVinculada(models.Model):
 
     atividade     = models.ForeignKey(Atividade, on_delete=models.PROTECT)
     professor     = models.ForeignKey(Professor, on_delete=models.PROTECT)
     dofertada     = models.ForeignKey(DisciplinaOfertada, on_delete=models.PROTECT)
 
-    rotulo        = models.TextField(max_length=500)  
+    rotulo        = models.TextField(max_length=500, unique=True)  
     status        = models.TextField(max_length=15)
-    dtiniresposta = models.DateTimeField()
-    dtfimresposta = models.DateTimeField()
+    dtiniresposta = models.DateTimeField(default=datetime.now, blank=True)
+    dtfimresposta = models.DateTimeField(default=datetime.now, blank=True)
 
+class Entrega(models.Model):
 
-class EntregaAtividade():
+    aluno               = models.ForeignKey(Aluno, on_delete=models.PROTECT)
+    professor           = models.ForeignKey(Professor, on_delete=models.PROTECT)
+    atividade_vinculada = models.ForeignKey(AtividadeVinculada, on_delete=models.PROTECT)
 
-    professor = models.ForeignKey(Professor, on_delete=models.PROTECT)
-    atividade = models.ForeignKey(Atividade, on_delete=models.PROTECT)
-    disciplina_ofertada = models.ForeignKey(DisciplinaOfertada, on_delete=models.PROTECT)
+    titulo              = models.TextField(max_length=100)
+    resposta            = models.TextField(max_length=8000)
+    dtentrega           = models.DateTimeField(default=datetime.now, blank=True)
+    dtavaliacao         = models.DateTimeField(default=datetime.now, blank=True, null=True)
+    status              = models.TextField(max_length=10)
+    nota                = models.DecimalField(max_digits=4,decimal_places=2, null=True)
+    obs                 = models.TextField(max_length=500, null=True)
 
 class Mensagem(models.Model):
  
@@ -105,9 +132,9 @@ class Mensagem(models.Model):
     referencia = models.TextField(max_length=100) 
     conteudo   = models.TextField(max_length=500) 
     status     = models.TextField(max_length=50) 
-    dtenvio    = models.DateField(default=date(year=1900, month=1, day=1))
-    dtresposta = models.DateField(default=date(year=1900, month=1, day=1))
-    resposta   = models.TextField(max_length=500)
+    dtenvio    = models.DateTimeField(default=datetime.now, blank=True)
+    dtresposta = models.DateTimeField(default=datetime.now, blank=True, null=True)
+    resposta   = models.TextField(max_length=500, null=True)
 
 
 
